8f6e367a77f8f80e20362023e0fc663a001f96b8
khanhhoang209 <lehoangkhanh209@gmail.com>
2025-11-12 17:47:04 +0700

    feat: add new migration

---
+++ src/Services/Exam/Exam.Domain/Entities/Exam.cs
@@ -0,0 +1,11 @@
+﻿namespace Exam.Domain.Entities;
+
+public class Exam : BaseAuditableEntity<Guid>
+{
+    public Guid SemesterId { get; set; }
+    public string Code { get; set; } = null!;
+    public DateTimeOffset StartDate { get; set; }
+    public DateTimeOffset EndDate { get; set; }
+
+    public Semester? Semester { get; set; } = null!;
+}
---
+++ src/Services/Exam/Exam.Repositories/Configurations/SubjectConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Exam.Domain.Entities;
+using Exam.Repositories.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Exam.Repositories.Configurations;
+
+public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+{
+    public void Configure(EntityTypeBuilder<Subject> builder)
+    {
+        builder.ToTable("Subject");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.SemesterId)
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+
+        builder.Property(x => x.Code)
+            .IsRequired();
+
+        builder.ConfigureAuditableEntity();
+    }
+}
